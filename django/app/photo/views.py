2fe11153cc260365838eaaf1528a5399cc4a55d2
Kelvin-Zhong <601414712zx@gmail.com>
2020-06-17 22:27:20 -0700

    add text to image

--- django/app/photo/views.py
+++ django/app/photo/views.py
@@ -9,6 +9,30 @@ from photo import serializers
 
 from deeplearning.face_expression_detector import FaceExpressionDetector
 
+from io import BytesIO
+
+from django.core.files.base import ContentFile
+
+from PIL import Image, ImageDraw, ImageFont
+
+FONT_PATH = 'photo/arial.ttf'
+
+
+def addTextToImg(img, text):
+    draw = ImageDraw.Draw(img)
+    font = ImageFont.truetype(FONT_PATH, size=45)
+    color = 'rgb(255, 255, 255)'  # white color
+    text_coordinates = (50, 50)
+    draw.text(text_coordinates, text, fill=color, font=font)
+    return img
+
+
+def saveImageToContentFile(img):
+    img_io = BytesIO()
+    img.save(img_io, format='png', quality=100)
+    img_content = ContentFile(img_io.getvalue())
+    return img_content
+
 
 class PhotoViewSet(viewsets.ModelViewSet):
     """Manage photo related API"""
@@ -45,6 +69,14 @@ class PhotoViewSet(viewsets.ModelViewSet):
             # label = 'unhappy'
             label = FacialExpressionType.UNHAPPY
         print("Predicted Label: ", label)
+
+        # Add label as text to image
+        img = Image.open(img_path)
+        img = addTextToImg(img, label)
+
+        # Save image to photo model
+        content_file = saveImageToContentFile(img)
+        photo_data.image.save(photo_data.image.name, content_file)
 
         photo_data.facial_expression_type = label
         photo_data.save()
